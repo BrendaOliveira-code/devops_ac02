677789aa0be0aaf54ae95335eaafe7da0c532b0f
BrendaOliveira-code <61034227+BrendaOliveira-code@users.noreply.github.com>
2020-04-08 19:15:10 -0300

    ADD ARQUIVOS

---
+++ com/brenda/test/test_operacoes.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from com.brenda.operacoes import Operacoes
+
+class TestOperacoes(TestCase):
+
+    def setUp(self):
+        self.operacoes = Operacoes()
+    
+    def test_soma(self):
+        self.assertEqual(self.operacoes.soma([26, 4]), 30, "O número é 30")
